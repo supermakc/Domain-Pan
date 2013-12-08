e3797e1adad589a426aaaa013dbcf5e3d3c5de53
Chris Davoren <cdavoren@gmail.com>
2013-12-08 12:43:58 +1000

    Added: Support for path specification in celermon script/executable.

--- celerymon/celerymon.c
+++ celerymon/celerymon.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
-int main() {
+#define SCRIPT_NAME "celerymon.sh"
+
+int main(int argc, char **argv) {
     int retval = 0;
+    char fullpath[1024];
+
+    strcpy(fullpath, argv[1]);
+    strcat(fullpath, SCRIPT_NAME);
+
     setuid(0);
-    /* system("/home/davorian/Code/celerymon/celerymon.sh"); */
-    retval = execl("/tmp/celerymon.sh", "celerymon.sh", NULL);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: celermon <path-of-celerymon.sh> [worker-prefix]\n");
+        fprintf(stderr, "   Path note: the trailing slash must be included.");
+        return 1;
+    }
+
+    if (argc > 2) {
+        retval = execl(fullpath, SCRIPT_NAME, argv[2], NULL);
+    }
+    else {
+        retval = execl(fullpath, SCRIPT_NAME, NULL);
+    }
+
     if (retval != 0) {
         fprintf(stderr, "Error occurred attempting to invoke script: %d\n", retval);
         return retval;
     }
+
     return 0;
 }
