b4c28f15040a88117db08bd1a01b48580e886c9a
Chris Davoren <cdavoren@gmail.com>
2013-12-17 10:17:11 +1000

    Fixed: Adjusted Celery monitoring scripts to account for celerybeat.

--- celerymon/celerymon.sh
+++ celerymon/celerymon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ `ps -ef | grep "python.*celery.*$1" | wc -l` -lt 2 ]
+if [ `ps -ef | grep "python.*celery.*worker.*$1" | wc -l` -lt 2 ]
 then
     echo '['`date`'] Celery processes appear to be down, restarting:' >&2
     rm -rf /tmp/namecheap.lock
@@ -8,3 +8,12 @@ then
 else
     echo '['`date`'] Celery processes appear to running, no restart required.' >&2
 fi
+
+if [ `ps -ef | grep "python.*celerybeat.*$2" | wc -l` -lt 2 ]
+then
+    echo '['`date`'] Celerybeat process appears to be down, restarting: ' >&2
+    /etc/init.d/celerybeat restart
+else
+    echo '['`date`'] Celerybeat process appears to be running, no restart required.' >&2
+fi
+
