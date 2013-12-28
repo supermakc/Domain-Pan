eda22c4a0142b0f99f71a3b967e88619d7f83c09
Chris Davoren <cdavoren@gmail.com>
2013-12-27 16:36:50 -0800

    Added:  Some more informative output to the syncmetrics command.

--- main/management/commands/syncmetrics.py
+++ main/management/commands/syncmetrics.py
@@ -6,15 +6,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         pdr = 0
+        pdra = 0
         pda = 0
+        pdaa = 0
         dur = 0
         for pd in ProjectDomain.objects.all():
             ums = URLMetrics.objects.filter(query_url=pd.domain).order_by('-last_updated')
             if len(ums) == 0:
                 pdr += 1
+                if pd.state == 'available':
+                    pdra += 1
                 pd.url_metrics = None
             else:
                 pda += 1
+                if pd.state == 'available':
+                    pdaa += 1
                 pd.url_metrics = ums[0]
             pd.save()
             if len(ums) > 1:
@@ -25,7 +31,9 @@ class Command(BaseCommand):
             p.update_state()
         self.stdout.write('Statistics:')
         self.stdout.write('  Linked domain entries: %d' % pda)
+        self.stdout.write('      Available: %d' % pdaa)
         self.stdout.write('  Unlinked domain entries: %d' % pdr)
+        self.stdout.write('      Available: %d' % pdra)
         self.stdout.write('  Duplicate metric records removed: %d' % dur)
         self.stdout.write('  Total metric records remaining: %d' % (len(URLMetrics.objects.all())))
 
